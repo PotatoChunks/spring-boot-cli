e32c1c000b8e6fe9f25457fa44bb970a53b18ef2
potatochunks <a2545585912@163.com>
2023-11-15 17:52:39 +0800

    修改sql文件

--- spring-boot-cli-sql.sql
+++ spring-boot-cli-sql.sql
@@ -18,22 +18,79 @@ DROP DATABASE IF EXISTS `my_demo_cli`;
 CREATE DATABASE IF NOT EXISTS `my_demo_cli` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
 USE `my_demo_cli`;
 
+DROP TABLE IF EXISTS `sms_member_role_relation`;
+CREATE TABLE `sms_member_role_relation` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `member_id` bigint DEFAULT NULL,
+  `role_id` bigint DEFAULT NULL,
+  `relation_type` int DEFAULT '1' COMMENT '1->管理员；2->用户；',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+DROP TABLE IF EXISTS `sms_resource`;
+CREATE TABLE `sms_resource` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `name` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '资源名称',
+  `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '资源URL',
+  `description` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '描述',
+  `category_id` bigint DEFAULT NULL COMMENT '资源分类ID',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='资源接口路径表';
+
+
+DROP TABLE IF EXISTS `sms_resource_category`;
+CREATE TABLE `sms_resource_category` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `name` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '分类名称',
+  `sort` int DEFAULT NULL COMMENT '排序',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='资源分类表';
+
+
+DROP TABLE IF EXISTS `sms_role`;
+CREATE TABLE `sms_role` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '角色名称',
+  `description` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '描述',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `status` int DEFAULT '1' COMMENT '启用状态：0->禁用；1->启用',
+  `sort` int DEFAULT '0' COMMENT '排序',
+  `role_type` int DEFAULT '1' COMMENT '角色类型：1->用户角色；2->后台角色；',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='用户角色表';
+
+
+DROP TABLE IF EXISTS `sms_role_resource_relation`;
+CREATE TABLE `sms_role_resource_relation` (
+  `role_id` bigint DEFAULT NULL COMMENT '角色ID',
+  `resource_id` bigint DEFAULT NULL COMMENT '资源ID'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='角色和资源关系表';
+
+
+
+
 -- 导出  表 my_demo_cli.ums_member_user_info 结构
 DROP TABLE IF EXISTS `ums_member_user_info`;
-CREATE TABLE IF NOT EXISTS `ums_member_user_info` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '用户名',
-  `phone` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '电话',
-  `user_id` bigint(20) DEFAULT NULL COMMENT '用户表id',
+CREATE TABLE `ums_member_user_info` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '用户名',
+  `phone` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '电话',
+  `user_id` bigint DEFAULT NULL COMMENT '用户表id',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='用户基础信息表';
 
+
 -- 导出  表 my_demo_cli.ums_member_user 结构
 DROP TABLE IF EXISTS `ums_member_user`;
-CREATE TABLE IF NOT EXISTS `ums_member_user` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `user_info_id` bigint(20) DEFAULT NULL COMMENT '用户信息表id',
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '密码',
+CREATE TABLE `ums_member_user` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `user_info_id` bigint DEFAULT NULL COMMENT '用户信息表id',
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '密码',
+  `user_code` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '唯一编码',
+  `status` int DEFAULT NULL COMMENT '状态：0->禁用；1->正常；',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='用户表';
 
