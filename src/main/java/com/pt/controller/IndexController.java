c2b707aea6ede562657891c577044e7996a18552
potatochunks <a2545585912@163.com>
2023-11-20 17:29:12 +0800

    新增ip工具包

--- src/main/java/com/pt/controller/IndexController.java
+++ src/main/java/com/pt/controller/IndexController.java
@@ -1,6 +1,9 @@
 package com.pt.controller;
 
 import com.pt.api.common.CommonResult;
+import net.dreamlu.mica.ip2region.core.Ip2regionSearcher;
+import net.dreamlu.mica.ip2region.core.IpInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,11 +12,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping(value = "/",produces = {"application/json;charset=UTF-8"})
 public class IndexController {
+    @Autowired
+    private Ip2regionSearcher ip2regionSearcher;
 
-    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    //ip获取地理位置用法
+    @RequestMapping(value = "/test/ip", method = RequestMethod.GET)
     @ResponseBody
-    public CommonResult<String> test(){
-        return CommonResult.success("OK");
+    public CommonResult getIpTest(){
+        IpInfo ipInfo = ip2regionSearcher.memorySearch("192.168.10.42");
+        //province省份
+        //city城市
+        //country国家
+        return CommonResult.success(ipInfo);
     }
 
 }
